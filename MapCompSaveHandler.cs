5f36bdf7f4ac11911e119b098fef632d8b93eee8
GHXX <GHXX@gmx.at>
2018-03-11 20:56:52 +0100

    removed some log lines that are unnecessary.

--- MapCompSaveHandler.cs
+++ MapCompSaveHandler.cs
@@ -64,20 +64,20 @@ namespace TechAdvancing
 
             Scribe_Collections.Look(ref Configvalues, "TA_Expose_Numbers", LookMode.Value, LookMode.Value);
             int isPplDictSaved = 1;
-            LogOutput.WriteLogMessage(Errorlevel.Information, "val:" + isPplDictSaved.ToString());
+            //LogOutput.WriteLogMessage(Errorlevel.Information, "val:" + isPplDictSaved.ToString());
             Scribe_Values.Look(ref isPplDictSaved, "TA_Expose_People_isSaved", -1, true);
-            LogOutput.WriteLogMessage(Errorlevel.Information, "val:" + isPplDictSaved.ToString());
+            //LogOutput.WriteLogMessage(Errorlevel.Information, "val:" + isPplDictSaved.ToString());
             if (isPplDictSaved == 1)
             {
                 Scribe_Collections.Look(ref ColonyPeople, "TA_Expose_People", LookMode.Reference, LookMode.Reference);
-                LogOutput.WriteLogMessage(Errorlevel.Information, "Read TA_ExposePeople");
+                //LogOutput.WriteLogMessage(Errorlevel.Information, "Read TA_ExposePeople");
             }
-            LogOutput.WriteLogMessage(Errorlevel.Information, "Loading");
             TechAdvancing_Config_Tab.ExposeData(TA_Expose_Mode.Load);
             if (ColonyPeople == null)
             {
                 ColonyPeople = new Dictionary<Pawn, Faction>();
             }
+            LogOutput.WriteLogMessage(Errorlevel.Information, "Loading finished.");
         }
     }
 
