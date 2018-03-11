750a7abdef4b83656e3d516cd6af3fd3bc09a1b4
GHXX <GHXX@gmx.at>
2018-03-11 21:04:39 +0100

    Changed assembly properties

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("GHXX")]
 [assembly: AssemblyProduct("TechAdvancing")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
