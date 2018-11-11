b46e0e3e8f522e79bd327dbe621dad639f5a92c0
GHXX <GHXX@gmx.at>
2018-11-11 19:34:37 +0100

    Potantially fixed null-key scribe loading error.

--- MapCompSaveHandler.cs
+++ MapCompSaveHandler.cs
@@ -67,6 +67,10 @@ namespace TechAdvancing
             //LogOutput.WriteLogMessage(Errorlevel.Information, "val:" + isPplDictSaved.ToString());
             Scribe_Values.Look(ref isPplDictSaved, "TA_Expose_People_isSaved", -1, true);
             //LogOutput.WriteLogMessage(Errorlevel.Information, "val:" + isPplDictSaved.ToString());
+            if (ColonyPeople != null)
+            {
+                ColonyPeople.RemoveAll(x => x.Key == null);
+            }
             if (isPplDictSaved == 1)
             {
                 Scribe_Collections.Look(ref ColonyPeople, "TA_Expose_People", LookMode.Reference, LookMode.Reference);
